0e86f0a16568798849c4c259e40e04e9f3e83a68
nikita <timonin_nikita@mail.ru>
2024-04-06 10:27:24 +0700

    add: add mutation, crossover, compute neurons fitness

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 
 from sane import Sane
 from utils import read_dt
@@ -8,21 +10,27 @@ np.random.seed(seed=42)
 hyperparameters = {
     "population_size": 1000,
     "hidden_neurons": 100,
-    "epoch": 100,
+    "epoch": 200,
     "neuron_connections": 7
 }
 
 
 def run():
-    x, y = read_dt('glass1.dt')
+    # x, y = read_dt('glass1.dt')
+
+    data = load_iris()
+    x = data["data"]
+    y = data["target"]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
     hyperparameters["input_neurons"] = x.shape[1]
     hyperparameters["output_neurons"] = np.unique(y).shape[0]
     print(hyperparameters)
     genetic = Sane(
         hyperparameters
     )
-    result = genetic.train(x)
-    # print(result[0])
+    result = genetic.train(x_train, y_train)
+    print(result[0])
 
 
 if __name__ == '__main__':
--- sane.py
+++ sane.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import log_loss
 
 from model import Model
 
@@ -13,9 +14,9 @@ class Sane:
         self.input_neurons = hyperparameters["input_neurons"]
         self.output_neurons = hyperparameters["output_neurons"]
 
-    def train(self, x_train):
+    def train(self, x_train, y_train):
         population = self.generate_population()
-
+        best_loss = np.inf
         curr_epoch = 0
         while curr_epoch < self.epoch:
             # 1. Обновление приспособленности нейронов
@@ -23,16 +24,73 @@ class Sane:
             # Количество вхождений нейронов в ИНС
             neurons_usage = np.ones(self.population_size)
 
-            for _ in range(100):
+            for _ in range(200):
                 # 2. Случайный выбор нейронов для сети
-                neurons_for_nn = np.random.randint(0, self.population_size, size=self.hidden_neurons)
+                neurons_in_nn = np.random.randint(0, self.population_size, size=self.hidden_neurons)
                 # Обновление количества вхождений для каждого нейрона
-                for neuron_id in np.unique(neurons_for_nn):
+                for neuron_id in np.unique(neurons_in_nn):
                     neurons_usage[neuron_id] += 1
-                network_schema = population[neurons_for_nn]
+                # 3. Создание НС
+                network_schema = population[neurons_in_nn]
                 model = Model(network_schema, self.hyperparameters)
-                model.forward_propagation(x_train)
+                # 4. Оценка сети / приспособленности нейронов, входящих в неё
+                network_predict = model.forward_propagation(x_train)
+                loss_nn = log_loss(y_train, network_predict)
+                if loss_nn < best_loss:
+                    print(curr_epoch, loss_nn)
+                    best_loss = loss_nn
+
+                # 5. Добавление приспособленности к использованным нейронам
+                neurons_fitness = self.update_neuron_fitness(neurons_fitness, neurons_in_nn, loss_nn)
+
+            # 7. Среднее значение приспособленности
+            neurons_fitness /= neurons_usage
+
+            # Сортировка нейронов по приспособленности
+
+            sort_ids = np.argsort(neurons_fitness)
+            population = population[sort_ids]
+            # neurons_fitness = neurons_fitness[sort_ids]
+
+            # 8. Одноточечный кроссинговер
+
+            self.crossover(population)
+
+            # Мутация
+
+            self.mutation(population)
+
             curr_epoch += 1
+
+    def mutation(self, population):
+        # Шанс мутации индекса нейрона
+        chance_mutation_index = 0.01
+        # Шанс мутации веса связи нейрона
+        chance_mutation_weight = 0.03
+        for i in range(len(population)):
+            for j in range(len(population[i])):
+                if j % 2 == 0 and np.random.random() < chance_mutation_index:
+                    population[i, j] = np.random.randint(0, self.input_neurons + self.output_neurons)
+                elif j % 2 != 0 and np.random.random() < chance_mutation_weight:
+                    population[i, j] = np.random.uniform(-0.5, 0.5)
+
+    def crossover(self, population):
+        # Кол-во нейронов, которые будут изменены в процессе скрещивания
+        neurons_crossover = int(0.125 * self.population_size)
+        for i in range(0, neurons_crossover):
+            first_neuron = np.random.randint(0, neurons_crossover)
+            second_neuron = np.random.randint(0, neurons_crossover)
+            # Точка разрыва
+            break_point = np.random.randint(1, population.shape[1] - 1)
+            if first_neuron != second_neuron:
+                population[-i] = np.concatenate((population[first_neuron, 0:break_point],
+                                                 population[second_neuron, break_point:]), axis=0)
+
+    @staticmethod
+    def update_neuron_fitness(neurons_fitness, neurons_in_nn, loss):
+        for i in np.unique(neurons_in_nn):
+            neurons_fitness[i] += loss
+        return neurons_fitness
 
     def generate_population(self):
         neurons_population = np.zeros(shape=(self.population_size, self.neuron_connections * 2))
